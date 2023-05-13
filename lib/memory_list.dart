b00deb002c612443db2f1defc18a86fcb2e80d47
Dilshan <tdm.dev2@gmail.com>
2023-05-13 05:14:52 -0700

    Update memory_list.dart

--- lib/memory_list.dart
+++ lib/memory_list.dart
@@ -1,8 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final memoryListProvider = StateNotifierProvider((ref) => MemoryList(List.filled(10, -1)));
+const memoryListSize = 12;
 
-const memoryListSize = 10;
+final memoryListProvider = StateNotifierProvider((ref) => MemoryList(List.filled(memoryListSize, -1)));
 
 enum Direction {right, left}
 
@@ -14,89 +14,45 @@ class  MemoryList extends StateNotifier<List<int>> {
   bool isMemoryListFilled = false;
 
   int previousIndex = -1;
-
   // ------->
   void addItemToRight(int num) {
     if (direction == Direction.right) {
       if (previousIndex == (memoryListSize-1)) {
         previousIndex = 0;
         state[previousIndex] = num;
+        state = state.toList();
       } else {
         previousIndex++;
-      state[previousIndex] = num;
+        state[previousIndex] = num;
+        state = state.toList();
       }
     } else {
       state[previousIndex] = num;
+      state = state.toList();
     }
     direction = Direction.right;
     print(state);
   }
 
+  // <---------
   void addItemToLeft(int num) {
     if (direction == Direction.left) {
       if (previousIndex == 0) {
         previousIndex = (memoryListSize-1);
         state[previousIndex] = num;
+        state = state.toList();
       } else {
         previousIndex--;
         state[previousIndex] = num;
+        state = state.toList();
       }
     } else {
       state[previousIndex] = num;
+      state = state.toList();
     }
     direction = Direction.left;
     print(state);
   }
-
- }
-    /*if (isMemoryListFilled == true) {
-      circularIndexIncrementation();
-      //state[previousIndex] = image;
-      state[previousIndex] = num;
-    } else {
-      //state[previousIndex] = image;
-      /*if (previousIndex == (memoryListSize-1)) {
-        previousIndex = MemoryListFillStatus.filled;
-      }*/
-      state[previousIndex] = num;
-      circularIndexIncrementation();
-      
-    }*/
-    /*if (d == Direction.init || d == Direction.left) {
-      d = Direction.right;
-      state[previousIndex] = num;
-      circularIndexIncrementation();
-    } else {
-      d = Direction.right;
-      state[previousIndex] = num;
-      circularIndexIncrementation();
-      
-    }
-    */
-    //print(state);
-  
-  // for inc and dec ,
-  // not needed for reading because index is stored on image list
-  /*void circularIndexIncrementation() {
-    print("circular indexIncremented call");
-    if (previousIndex == memoryListSize-1) {
-      previousIndex = 0;
-    } else {
-      previousIndex++;
-    }
-  }*/
-
-  // <-------
-  
-  
-  
-
-  /*void circularIndexDecrementation() {
-    if (previousIndex == 0) {
-      previousIndex = memoryListSize - 1;
-    } else {
-      previousIndex--;
-    }
-  }*/
+}
 
 
